61999ac2c1135d339ca93c38810f789a0db58031
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-24 17:12:14 -0300

    new Exception

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -8,53 +9,21 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) {
         Connection  conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-        PreparedStatement preparedStatement = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        PreparedStatement st = null;
+
 
         try {
             conn = DB.getConnection();
-            st = conn.createStatement();
+            st = conn.prepareStatement("DELETE FROM department " +
+                    "WHERE " +
+                    "Id = ?");
+            st.setInt(1, 2);
 
-            rs = st.executeQuery("select * from department");
-
-            while (rs.next()){
-                System.out.println(rs.getInt("Id")+ ", " + rs.getString("Name"));
-            }
-
-            preparedStatement = conn.prepareStatement(
-                    "insert into seller " +
-                            "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                            "values (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, "Carl Purple");
-            preparedStatement.setString(2, "carl.purple@gmail.com");
-            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            preparedStatement.setDouble(4, 3000.0);
-            preparedStatement.setInt(5, 4);
-
-            int rowsAffected = preparedStatement.executeUpdate();
-            if(rowsAffected > 0){
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-                while (rs.next()){
-                    int id = resultSet.getInt("1");
-                    System.out.println("Done! Id= " + id);
-                }
-            }
-            else{
-                System.out.println("No rows affected");
-            }
-
+            int rowsAffected = st.executeUpdate();
             System.out.println("Rows affected: " + rowsAffected);
 
         } catch (SQLException e) {
-            throw new DbException(e.getMessage());
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        } finally {
-            DB.closeResultSet(rs);
-            DB.closeStatement(st);
-            DB.closeConnection();
+            throw new DbIntegrityException(e.getMessage());
         }
     }
 }
