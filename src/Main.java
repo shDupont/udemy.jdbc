32fb7599893bf5445b914c70597593655748c995
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-24 17:22:58 -0300

    Rolling back

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,39 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) {
         Connection  conn = null;
-        PreparedStatement st = null;
-
-
+        Statement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement("DELETE FROM department " +
-                    "WHERE " +
-                    "Id = ?");
-            st.setInt(1, 2);
+            conn.setAutoCommit(false);
 
-            int rowsAffected = st.executeUpdate();
-            System.out.println("Rows affected: " + rowsAffected);
+            st = conn.createStatement();
+
+            int row = st.executeUpdate("UPDATE seller SET BaseSalary = 4090 Where DepartmentId = 1");
+
+            int x = 1;
+            if (x < 2){
+                throw new SQLException("Fake error");
+            }
+
+            int row2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 Where DepartmentId = 2");
+
+            conn.commit();
+            System.out.println("Row1: " + row);
+            System.out.println("Row2: " + row2);
 
         } catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
+
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back!" + e.getMessage());
+            }
+            catch (SQLException e1){
+                throw new DbException("Error rolled back!" + e1.getMessage());
+            }
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
         }
     }
 }
